532431a254aa2312e1f4569de3e5946f62c8426b
Vishal Dubey <hdubey64@gmail.com>
2022-11-20 12:07:25 +0000

    Theme Color Add

---
+++ five-themeswitcher/src/Color.js
@@ -0,0 +1,14 @@
+const AppTheme = {
+   light: {
+      textColor: "#000",
+      backgroundColor: "#fff",
+      border: "2px dashed #000",
+   },
+   dark: {
+      textColor: "#fff",
+      backgroundColor: "#000",
+      border: "2px dashed #333",
+   },
+};
+
+export default AppTheme;
---
+++ five-themeswitcher/src/Components/ThemeToggler.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import ThemeContext from "../Context/ThemeContext";
+
+const ThemeToggler = () => {
+   const [themeMode, setThmeMode] = useContext(ThemeContext);
+
+   return (
+      <div
+         onClick={() => {
+            setThmeMode(themeMode === "light" ? "dark" : "light");
+         }}
+      >
+         <span> {themeMode === "light" ? "Turn Off " : "Lights On"} </span>
+      </div>
+   );
+};
+
+export default ThemeContext;
