04863f1bc96fc736f85f612aa52486ec16693731
Vishal Dubey <hdubey64@gmail.com>
2022-11-25 12:01:14 +0000

    Todos File Created

--- six-context-reducer-todo/src/Components/TodoForms.js
+++ six-context-reducer-todo/src/Components/TodoForms.js
@@ -1,13 +1,31 @@
-import React, { useSate, useContext } from "react";
-import { FormGroup, Input, Button, Form, InputGroup, InputG } from "reactstrap";
+import React, { useContext, useState } from "react";
+import { FormGroup, Input, Button, Form, InputGroup } from "reactstrap";
 
 import { v4 } from "uuid";
 import { TodoContext } from "../Context/TodoContext";
 import { ADD_TODO } from "../Context/action.types";
 
 const TodoForm = () => {
+   const [todoString, setTodoString] = useState("");
+   const { dispatch } = useContext(TodoContext);
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      if (todoString === "") {
+         return "Plese Enter A Todo";
+      }
+      const todo = {
+         todoString,
+         id: v4(),
+      };
+      dispatch({
+         type: ADD_TODO,
+         payload: todo,
+      });
+
+      setTodoString("");
+   };
    return (
-      <Form>
+      <Form onSubmit={handleSubmit}>
          <FormGroup>
             <InputGroup>
                <Input
@@ -15,7 +33,8 @@ const TodoForm = () => {
                   name="todo"
                   id="todo"
                   placeholder="Your next Todo"
-                  // TODO: value. onChange
+                  value={todoString}
+                  onChange={(e) => setTodoString(e.target.value)}
                />
                <Button
                   color="warning"
---
+++ six-context-reducer-todo/src/Components/Todos.js
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { FacheckDouble } from "react-icon/fa";
+import { TodoContext } from "../Context/TodoContext";
+import { REMOVE_TODO } from "../Context/action.types";
+
+const Todos = () => {};
