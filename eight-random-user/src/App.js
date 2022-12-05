4b824fca7c6431ece58b2f65de873a626d672d17
Vishal Dubey <hdubey64@gmail.com>
2022-12-05 11:18:54 +0000

    Issue in port

--- eight-random-user/src/App.js
+++ eight-random-user/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffact } from "react";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [details, setDetails] = useState({});
+
+   const fetchDetails = async () => {
+      const response = await Axios.get("https://randomuser.me/api/");
+      console.log("RESPONSE", response);
+   };
+
+   useEffact(() => {}, []);
+
+   return (
+      <div className="App">
+         <header className="App-header">
+            <p>
+               Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default App;
