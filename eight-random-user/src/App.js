c8c00d06fc0e7f73393981acc6440d6d6c9d194a
hdubey64 <hdubey64@gmail.com>
2022-12-07 17:56:40 +0530

    MyCard created

--- eight-random-user/src/App.js
+++ eight-random-user/src/App.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Card } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Axios from "axios";
+import MyCard from "./MyCard";
 
 function App() {
    const [details, setDetails] = useState({});
 
    const fetchDetails = async () => {
-      const response = await Axios.get("https://randomuser.me/api/");
-      console.log("RESPONSE", response);
+      const { data } = await Axios.get("https://randomuser.me/api/");
+      console.log("RESPONSE", data);
+
+      const details = data.results[0];
+
+      setDetails(details);
    };
 
-   useEffect(() => {}, []);
+   useEffect(() => {
+      fetchDetails();
+   }, []);
 
    return (
-     //
+      <Container fluid className="p-4 bg-primary App">
+         <Row>
+            <Col md={4} className="offset-md-4 mt-4">
+               <MyCard details={details} />
+            </Col>
+         </Row>
+      </Container>
    );
 }
 
