1493987c9949808e8a2e4a856b97a37f484a0667
hdubey64 <hdubey64@gmail.com>
2022-12-27 17:04:20 +0530

    New File Header Created

--- ten-firebase-github-app/src/Layout/Header.js
+++ ten-firebase-github-app/src/Layout/Header.js
@@ -9,3 +9,44 @@ import {
    NavLink,
    NavbarText,
 } from "reactstrap";
+
+import { Link } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
+
+const Header = () => {
+    const context = useContext{UserContext}
+
+    const
+       return (
+      <Navbar color="info" ligh expand="md">
+         {" "}
+         <NavbarBrand>
+            <Link to="/" className="text-white">
+               LCO gitfire App
+            </Link>
+         </NavbarBrand>
+         <NavbarToggler />
+         <Collapse navbar>
+            <Nav className="ms-auto">
+               <NavItem>
+                  <NavLink tag={Link} to="/" className="text-white">
+                     Sign In
+                  </NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink tag={Link} to="/" className="text-white">
+                     Sign Up
+                  </NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink tag={Link} to="/" className="text-white">
+                     Logout
+                  </NavLink>
+               </NavItem>
+            </Nav>
+         </Collapse>
+      </Navbar>
+   );
+};
+
+export default Header;
