c0cb3fafd201b65ecd2850dcec6eac16d5db268b
hdubey64 <hdubey64@gmail.com>
2022-12-27 14:05:37 +0530

    Imported All Pages

--- ten-firebase-github-app/src/App.js
+++ ten-firebase-github-app/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//react-router
+import {
+   BrowserRouter as Router,
+   Switch,
+   Routes,
+   Route,
+   Link,
+} from "react-router-dom";
+// import { Routes, Route, Outlet, Link } from "react-router-dom";
+//toast
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.min.css";
+
+//firebase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+//components
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import PageNotFound from "./Pages/PageNotFound";
+import { UserContext } from "./Context/UserContext";
+
+const App = () => {
+   const [user, setUser] = useState(null);
+
+   return (
+      <Router>
+         <ToastContainer />
+         <UserContext.Provider value={{ user, setUser }}>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route exact path="/signin" element={<SignIn />} />
+               <Route exact path="/signup" element={<SignUp />} />
+               <Route exact path="*" element={<PageNotFound />} />
+            </Routes>
+         </UserContext.Provider>
+      </Router>
+   );
+};
 
 export default App;
